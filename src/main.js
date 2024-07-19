2f8a0fbc005a54eb4d11d40e35b8daf47505a9b3
Ruvick <rudikov.web@ya.ru>
2024-07-19 12:01:44 +0300

    Стартовые файлы

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// import './assets/main.css'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
